6277e658412695df64751ae895a96b1875071c67
IvafaN <46545984+IvafaN@users.noreply.github.com>
2021-03-26 01:41:27 -0700

    fivances

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Why choose a Fivances Credit Card?',
+    headline: 'Fivances offers you opportunities to earn a variety of rewards',
+    description:
+      'From Cash Back to AIR MILES®* reward miles. The benefits do not stop there. Many of our Credit Cards offer you Purchase Protection® Plan1+, Fraud Protection Guarantee2, travel-related insurance benefits.',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+  };
+  
+  export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '100% SECURE',
+    headline: '24/7 customer support',
+    description:
+      'If you want us, there is always someone to speak with you, 24/7, about whatever you need big or small. Wherever you are in the world.',
+    buttonLabel: 'Learn More',
+    imgStart: '',
+    img: 'images/svg-5.svg',
+    alt: 'Vault'
+  };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'ELIGIBILITY',
+    headline: 'Before we can consider your application, please ensure you can say yes to the following:',
+    description:
+      "You are a Canadian resident and have a Canadian credit file. You are of the age of majority in the province or territory of the residence",
+    buttonLabel: 'Start Now',
+    imgStart: 'start',
+    img: 'images/svg-7.svg',
+    alt: 'Vault'
+  };
+  
+  export const homeObjFour = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'MANAGE YOUR ACCOUNT ONLINE',
+    headline: 'With Online Services from Fivances',
+    description:
+      'You can stay on top of your account. Check statements, set account alerts and more while you are on the go.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: 'images/svg-8.svg',
+    alt: 'Vault'
+  };
